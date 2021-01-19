5efb6dffe2af8a6fb2fd7c66b2efb610921851b3
Matheus Curvelo <38927923+matheuscurvelo@users.noreply.github.com>
2021-01-18 22:13:47 -0300

    update sql

--- aulas/autoload/Classes/Sql.php
+++ aulas/autoload/Classes/Sql.php
@@ -1,33 +1,36 @@
 <?php
+    <pre>
     class Sql extends PDO{
         
         private $conn;
         
         function __Construct(){
-            $this->conn = new PDO("mysql:host=localhost;dbname=db_gie","mmcs","");
+            $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","root");
         }
 
-        private function setParams($stmt,$params){
+        private function setParams($comando,$params){
+            $stmt = $this->conn->prepare($comando);
             foreach ($params as $key => $value) {
                 $stmt->bindParam($key,$value);
+                //echo "$key => $value<br>";
             }
-        }
-
-        public function query($comando,$params = []){
-            $stmt = $this->conn->prepare($comando);
-            $this->setParams($stmt,$params);
-            $stmt->execute();
+            echo $stmt->execute();
             return $stmt;
         }
 
+        private function comando($comando,$params = array()){
+            $this->setParams($comando,$params);
+        }
+
         public function select($comando,$params = []): array{
-            $stmt = $this->query($comando,$params);
+            $stmt = $this->setParams($comando,$params);
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
 
-        public function insert($comando,$params = []){
-            $this->query($comando,$params);
+        public function insert($comando,$params = array()){
+            $this->comando($comando,$params);
             return $this->conn->lastInsertId();
         }
     }
+    </pre>
 ?>
--- aulas/autoload/index.php
+++ aulas/autoload/index.php
@@ -1,11 +1,10 @@
 <?php
     require 'config.php';
     $obj = new Sql();
-    echo json_encode($obj->select("SELECT * FROM status_servico"));
-    echo $obj->insert("INSERT INTO status_servico(tipo,descricao,desc_alt) VALUES (?,?,?)",array(
-        '1'=>'E',
-        '2'=>'teste',
-        '3'=>'testando')
+    //echo ($obj->select("SELECT * FROM tb_usuarios"));
+    $obj->insert("INSERT INTO tb_usuarios(deslogin,dessenha) VALUES (?,?)",[
+        1=>'marcos',
+        2=>'123']
     );
     
 ?>
