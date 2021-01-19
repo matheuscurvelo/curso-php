68cbf0b427e95d1ff81d58542a0ce6d12b9c53cb
Matheus Curvelo <38927923+matheuscurvelo@users.noreply.github.com>
2021-01-19 00:13:50 -0300

    DAO básico genérico
    DAO com comandos de consulta, inserção, atualização e remoção de registros

--- aulas/autoload/Classes/Sql.php
+++ aulas/autoload/Classes/Sql.php
@@ -1,5 +1,4 @@
 <?php
-    <pre>
     class Sql extends PDO{
         
         private $conn;
@@ -8,29 +7,61 @@
             $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","root");
         }
 
-        private function setParams($comando,$params){
-            $stmt = $this->conn->prepare($comando);
-            foreach ($params as $key => $value) {
-                $stmt->bindParam($key,$value);
-                //echo "$key => $value<br>";
+        private function setParams($statement, $parameters = array()){
+
+            foreach ($parameters as $key => $value) {
+                
+                $this->setParam($statement, $key, $value);
+    
             }
-            echo $stmt->execute();
+    
+        }
+    
+        private function setParam($statement, $key, $value){
+    
+            $statement->bindParam($key, $value);
+    
+        }
+    
+        public function query($rawQuery, $params = array()){
+    
+            $stmt = $this->conn->prepare($rawQuery);
+    
+            $this->setParams($stmt, $params);
+    
+            $stmt->execute();
+            
             return $stmt;
+    
         }
-
-        private function comando($comando,$params = array()){
-            $this->setParams($comando,$params);
-        }
-
-        public function select($comando,$params = []): array{
-            $stmt = $this->setParams($comando,$params);
+    
+        public function select($rawQuery, $params = array()):array
+        {
+    
+            $stmt = $this->query($rawQuery, $params);
+    
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
         }
 
-        public function insert($comando,$params = array()){
-            $this->comando($comando,$params);
+        public function insert($rawQuery, $params = array()):int
+        {
+    
+            $stmt = $this->query($rawQuery, $params);
             return $this->conn->lastInsertId();
+    
+        }
+
+        public function update($rawQuery, $params = array()):int
+        {
+            $stmt = $this->query($rawQuery, $params);
+            return $stmt->rowCount();
+        }
+    
+        public function delete($rawQuery, $params = array()):int
+        {
+            $stmt = $this->query($rawQuery, $params);
+            return $stmt->rowCount();
         }
     }
-    </pre>
 ?>
--- aulas/autoload/index.php
+++ aulas/autoload/index.php
@@ -1,10 +1,25 @@
 <?php
     require 'config.php';
     $obj = new Sql();
-    //echo ($obj->select("SELECT * FROM tb_usuarios"));
-    $obj->insert("INSERT INTO tb_usuarios(deslogin,dessenha) VALUES (?,?)",[
-        1=>'marcos',
-        2=>'123']
-    );
     
+    //Select - Retorna um array
+    //echo ($obj->select("SELECT * FROM tb_usuarios"));
+
+    //Insert - Retorna o ID inserido
+    /*$obj->insert("INSERT INTO tb_usuarios(deslogin,dessenha) VALUES (?,?)",[
+        1=>'marcos',
+        2=>'123'
+    ]);*/
+
+    //Update - Retorna o numero de linhas afetadas
+    /*echo $obj->update("UPDATE tb_usuarios SET deslogin= ?, dessenha= ? WHERE idusuario= ?",[
+        1=>'joao',
+        2=>'456',
+        3=>5
+    ]);*/
+
+    //Delete
+    echo $obj->delete("DELETE FROM tb_usuarios WHERE idusuario= ?",[
+        1=>6
+    ]);
 ?>
